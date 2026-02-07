0a339e3b31cbe8e22db7360739f64e401c8dd0ee
Tomasz Pływacz <tomek@tomaszs-macbook-pro.home>
2026-02-07 13:28:09 +0100

    implement compress folder funcion

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@
 #include "Decoder.h"
 #include <fstream>
 #include <algorithm>
+#include <filesystem>
 
-std::vector<uint8_t> pack_file(const std::string& file_path, const std::string& cut_file_path) {
+namespace fs = std::filesystem;
+
+std::vector<uint8_t> pack_file(fs::path& file_path, fs::path& cut_file_path) {
     std::ifstream file(file_path, std::ios::binary | std::ios::ate);
     if (!file) {
         std::cout << "Unable to open file" << file_path << "\n";
@@ -23,16 +26,35 @@ std::vector<uint8_t> pack_file(const std::string& file_path, const std::string& 
     unsigned int comp_size = compressed.size();
     uint8_t* comp_size_bytes = (uint8_t*)&comp_size;
     std::vector<uint8_t> res = compressed;
-    for(int i = 0; i < 4; i ++) {
+    for(int i = 3; i >= 0; i --) {
         res.push_back(comp_size_bytes[i]);
     }
-    for(int i = 0; i < cut_file_path.size(); i++) {
-        res.push_back(cut_file_path[i]);
+    std::string cut_file_path_str = cut_file_path.string();
+    for(int i = cut_file_path_str.size() - 1; i >= 0; i--) {
+        res.push_back(cut_file_path_str[i]);
+    }
+    unsigned int l = cut_file_path_str.size();
+    uint8_t* l_bytes = (uint8_t*)&l;
+    for(int i = 3; i >= 0; i--) {
+        res.push_back(l_bytes[i]);
     }
     std::reverse(res.begin(), res.end());
     return res;
 }
 
+std::vector<uint8_t> pack_folder(const fs::path& folder_path) {
+    std::vector<uint8_t> res;
+    for(auto& file : fs::directory_iterator(folder_path)) {
+        if(file.is_regular_file()) {
+            fs::path file_path = file.path();
+            fs::path cut_file_path = file.path();
+            cut_file_path = cut_file_path.lexically_relative(folder_path);
+            std::vector<uint8_t> curr = pack_file(file_path, cut_file_path);
+            res.insert(res.end(), curr.begin(), curr.end());
+        }
+    }
+    return res;
+}
 int main() {
     std::string testString = "ABR";
     while(testString.size() < 10000) {
